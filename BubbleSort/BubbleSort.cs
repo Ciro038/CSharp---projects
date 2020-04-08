d9e64813c2f1a90189499836492a61db9412cb60
Ciro038 <62670145+Ciro038@users.noreply.github.com>
2020-04-08 19:40:40 +0200

    Add files via upload
    Program sorting table using bubble sort method.

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    static class BubbleSort
+    {
+        public static void BubbleProgram()
+        {
+            Console.WriteLine("Hello in Bubble sort program! :D\nGive me how much elements have yours table:");
+            int tabNumber = int.Parse(Console.ReadLine());
+            int[] tab = new int[tabNumber];
+            for (int i = 0; i < tabNumber; i++)
+            {
+                Console.WriteLine($"Give me {i+1} element of yours table: ");
+                tab[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.Clear();
+            Console.Write("Your table: ");
+            foreach (int item in tab)
+            {
+                Console.Write(item+"\t");
+            }
+            Console.Write("\n");
+            bool shouldRepeat;
+            do
+            {
+                shouldRepeat = false;
+                for (int i = 0; i < tab.Length-1; i++)
+                {
+                    int currentElement = tab[i];
+                    int nextElement = tab[i + 1];
+                    if (currentElement>nextElement)
+                    {
+                        shouldRepeat = true;
+                        tab[i] = nextElement;
+                        tab[i + 1] = currentElement;
+                    }
+                }
+            } while (shouldRepeat);
+            Console.Write("Your sorted table: ");
+            foreach (int item in tab)
+            {
+                Console.Write(item+"\t");
+            }
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    /*
+     * Sorting program using bubble sort method
+     * -program use only int table
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort.BubbleProgram();
+            Console.ReadKey();
+        }
+    }
+}
